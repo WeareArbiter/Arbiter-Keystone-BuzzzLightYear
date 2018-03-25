0eaabcec50eed121b1181f7ac074aec8c01a98e5
artlyhub <ppark9553@gmail.com>
2018-03-25 14:39:02 +0900

    adding admin page options

--- stockapi/admin.py
+++ stockapi/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
-# Register your models here.
+from stockapi.models import (
+    BM,
+    Ticker,
+    KospiOHLCV,
+    KosdaqOHLCV,
+    RecentKospiOHLCV,
+    RecentKosdaqOHLCV,
+    Info,
+    Specs,
+    Financial,
+    FinancialRatio,
+    QuarterFinancial,
+    KospiBuy,
+    KosdaqBuy,
+    ETFBuy,
+    KospiSell,
+    KosdaqSell,
+    ETFSellm
+    KospiNet,
+    KosdaqNet,
+    ETFNet,
+)
+
+models = [
+    BM,
+    Ticker,
+    KospiOHLCV,
+    KosdaqOHLCV,
+    RecentKospiOHLCV,
+    RecentKosdaqOHLCV,
+    Info,
+    Specs,
+    Financial,
+    FinancialRatio,
+    QuarterFinancial,
+    KospiBuy,
+    KosdaqBuy,
+    ETFBuy,
+    KospiSell,
+    KosdaqSell,
+    ETFSellm
+    KospiNet,
+    KosdaqNet,
+    ETFNet,
+]
+for model in models:
+    admin.site.register(model)
--- stockapi/views.py
+++ stockapi/views.py
@@ -53,6 +53,7 @@ from utils.paginations import StandardResultPagination, OHLCVPagination
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 
+### KOSPI & KOSDAQ data ###
 class BMAPIView(generics.ListCreateAPIView):
     queryset = BM.objects.all()
     serializer_class = BMSerializer
@@ -75,4 +76,27 @@ class BMAPIView(generics.ListCreateAPIView):
             queryset = queryset.filter(name=name_by)
         if category_by:
             queryset = queryset.filter(category=category_by)
+        return queryset
+
+
+class TickerAPIView(generics.ListCreateAPIView):
+    queryset = Ticker.objects.all()
+    serializer_class = TickerSerializer
+    pagination_class = StandardResultPagination
+    filter_backends = [SearchFilter, OrderingFilter]
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Ticker.objects.all().order_by('id')
+        date_by = self.request.GET.get('date')
+        code_by = self.request.GET.get('code')
+        name_by = self.request.GET.get('name')
+        market_by = self.request.GET.get('market_type')
+        if date_by:
+            queryset = queryset.filter(date=date_by)
+        if name_by:
+            queryset = queryset.filter(name=name_by)
+        if code_by:
+            queryset = queryset.filter(code=code_by)
+        if market_by:
+            queryset = queryset.filter(market=market_by)
         return queryset
