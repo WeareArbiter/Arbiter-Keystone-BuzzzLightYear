7109667aad7281f8cd5e6b29d9c93f985836dc98
lmh <MH-Lee>
2018-04-13 20:47:34 +0900

    test db change postgres

--- portfolio/api/serializers.py
+++ portfolio/api/serializers.py
@@ -1,12 +1,14 @@
-import time, datetime
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.serializers import ValidationError
 
-from portfolio.models import Portfolio, PortfolioItem
+from portfolio.models import (
+    Portfolio,
+    PortfolioItem,
+    PortfolioSpecs,
+    )
 
 User = get_user_model()
-
 
 class PortfolioSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,3 +33,24 @@ class PortfolioItemSerializer(serializers.ModelSerializer):
                   'status',
                   'quantity',
                   'price',)
+
+class PortfolioSpecsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PortfolioSpecs
+        fields = ('portfolio',
+                  'date',
+                  'portfolio_ratio',
+                  'ret',
+                  'avg_ret',
+                  'avg_vol',
+                  'sharp_ratio',
+                  'w_ret',
+                  'm_ret',
+                  'q_ret',
+                  'h_ret',
+                  'y_ret',
+                  'kp_w_ret',
+                  'kp_m_ret',
+                  'kp_q_ret',
+                  'kp_h_ret',
+                  'kp_y_ret',)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 from accounts.models import Profile
@@ -40,10 +41,34 @@ class Portfolio(models.Model):
 class PortfolioItem(models.Model):
     portfolio = models.ForeignKey(Portfolio, related_name='item')
     date = models.CharField(max_length=8)
-    code = models.ForeignKey(Ticker, related_name='portfolio_record')
+    code = models.ForeignKey(Ticker, related_name = 'portfolio_record')
     status = models.CharField(max_length=1, choices=STATUS)
     quantity = models.IntegerField(default=1)
     price = models.IntegerField()
 
     def __str__(self):
         return self.portfolio.name
+
+class PortfolioSpecs(models.Model):
+    portfolio = models.ForeignKey(Portfolio,
+                                  on_delete=models.CASCADE,
+                                  related_name = 'portfolio_ret')
+    date = models.CharField(max_length=10)
+    portfolio_ratio = JSONField()
+    ret = models.FloatField(blank=True, null=True)
+    avg_ret = models.FloatField(blank=True, null=True)
+    avg_vol = models.FloatField(blank=True, null=True)
+    sharp_ratio = models.FloatField(blank=True, null=True)
+    w_ret = models.FloatField(blank=True, null=True)
+    m_ret = models.FloatField(blank=True, null=True)
+    q_ret = models.FloatField(blank=True, null=True)
+    h_ret = models.FloatField(blank=True, null=True)
+    y_ret = models.FloatField(blank=True, null=True)
+    kp_w_ret = models.FloatField(blank=True, null=True)
+    kp_m_ret = models.FloatField(blank=True, null=True)
+    kp_q_ret = models.FloatField(blank=True, null=True)
+    kp_h_ret = models.FloatField(blank=True, null=True)
+    kp_y_ret = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.date, self.portfolio.user.user)
